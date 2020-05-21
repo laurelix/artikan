e81391bf076b632d533069546f7eb375a655bf23
laurelix <47852467+laurelix@users.noreply.github.com>
2020-05-21 22:00:10 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,26 @@
+const googleKey = 'AIzaSyBW6OtKAyke7Z3xrblKG3tR-wDcG7xs9PQ'
+  .split('')
+  .reverse()
+  .join('')
+
+export const apiUrls = {
+  cloudVision:
+    'https://vision.googleapis.com/v1/images:annotate?key=' + googleKey,
+  translate: 'https://www.googleapis.com/language/translate/v2?key=' + googleKey
+}
+
+const queryLangs = window.location.search.slice(1)
+
+export const langList = queryLangs
+  ? queryLangs.split(',')
+  : [
+      'spanish',
+      'french',
+      'german',
+      'italian',
+      'chinese',
+      'japanese',
+      'korean',
+      'hindi',
+      'dutch'
+    ]
